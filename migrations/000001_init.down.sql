147738e7f51f285b90770e84c315419354a2aea7
Lewis Injai <lewiskunta40@gmail.com>
2026-05-29 13:19:20 +0300

    setup: wire golang-migrate runner into server startup

--- migrations/000001_init.down.sql
+++ migrations/000001_init.down.sql
@@ -1,2 +1,4 @@
--- Reversal of initial migration.
-SELECT 1;
+-- Reversal of migration 000001.
+-- Removes the pgcrypto extension.
+-- Note: only safe to run if no tables using gen_random_uuid() exist.
+DROP EXTENSION IF EXISTS "pgcrypto";
